55e7c262d972a95e78b7f8d9889b0c133a81ccc7
Abdul  Ismail <abdulismail@Abduls-MacBook-Air.local>
2018-11-09 16:14:16 -0600

    working on adding total monthly

--- client.js
+++ client.js
@@ -5,10 +5,11 @@ $( document ).ready(readyNow);
 function readyNow() {
     console.log('Jquery working!');
     $('#submit').on('click', addEmployees);
+
     
 }// end of readyNow function 
 // class for employees 
-class Employess{
+class Employees{
     constructor( firstName, lastName, id, title, annualSalary ){
         this.firstName = firstName;
         this.lastName = lastName;
@@ -23,11 +24,44 @@ let employees = [];
 // this function will add employess to the table 
 function addEmployees() {
     console.log(' in addEmployees');
-    const tempEmp = new Employess( 
-        $( 'firstNameIn' ).val(),
-        $( 'lastNameIn' ).val(),
-        $( 'idIn' ).val(),
-        $( 'titleIn' ).val(),
-        $( 'annualSalaryIn' ).val()
+    const newEmp = new Employees( 
+        $( '#firstNameIn' ).val(),
+        $( '#lastNameIn' ).val(),
+        $( '#idIn' ).val(),
+        $( '#titleIn' ).val(),
+        $( '#annualSalaryIn' ).val()
     )
-}
+    console.log('added', newEmp);
+    employees.push( newEmp );
+    // emptying inputs 
+    $( '#firstNameIn' ).empty(' ');
+    $( '#lastNameIn' ).empty(' ');
+    $( '#idIn' ).empty(' ');
+    $( '#titleIn' ).empty(' ');
+    $( '#annualSalaryIn' ).empty(' ');
+    displayEmp(employees);
+}// end of addEmployees function 
+// This function will display employee information on the dom 
+function displayEmp(array) {
+    console.log(' in displayEmp');
+    
+    $('#output').empty();
+
+    for( let employee of array ){
+        $('#output').append( `<tr>
+        <td>${employee.firstName}</td>
+        <td>${employee.lasttName}</td> 
+        <td>${employee.id}</td> 
+        <td>${employee.title}</td>, 
+        <td>${employee.annualSalary}</td></tr>`
+    )
+    }// end of loop
+    // totalMonthlyIncome();
+} // end of displayEmp function 
+// function to calculate total Monthly Income 
+// function totalMonthlyIncome() {
+//     console.log('totals working');
+//     let totals;
+//     if( employee.annualSalary )
+
+// }
